5342c0ac9559cb569f7f4518402de95547676b5c
Dreamo <kyusaki1@gmail.com>
2021-03-27 17:49:03 +0200

    Fix now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'build')))
@@ -8,7 +9,12 @@ app.use(express.static(path.join(__dirname, 'build')))
 // load the value in the server
 const { API_URL } = process.env;
 
-app.get('/*', function (req, res) {
+const corsOptions = {
+  origin: API_URL,
+  optionsSuccessStatus: 200 
+}
+
+app.get('/*', cors(corsOptions), function (req, res) {
   res.render(path.join(__dirname, 'build', 'index.html'), {API_URL})
 })
 
