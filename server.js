70f3fc6373f3810899701e8d299838b2929a92c5
Dreamo <kyusaki1@gmail.com>
2021-03-27 16:37:06 +0200

    Try fix for API_URL

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const path = require('path')
-const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use(cors(express.static(path.join(__dirname, 'build'))))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
